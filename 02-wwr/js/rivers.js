ca2ae42c0ccc5d13af0d4c7c0173ede442bf55ee
tumbokon.aaronjulius@outlook.com <tumbokon.aaronjulius@outlook.com>
2022-11-07 19:46:03 -0700

    Added the Rivers html

---
+++ 02-wwr/js/rivers.js
@@ -0,0 +1,14 @@
+let tabcontent = document.getElementById("riv_card");
+console.log(tabcontent);
+// for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+// }
+
+
+function next() {
+    console.log("hi");
+}
+
+function prev() {
+    console.log("hello");
+}
